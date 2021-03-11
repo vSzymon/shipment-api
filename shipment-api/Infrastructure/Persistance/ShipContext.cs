d62faf0538cfc5d37325d666b9d17edbccf39e82
vszymon <vszymonwrobel@gmail.com>
2021-03-11 20:58:46 +0100

    feat: DbContext implementation added

---
+++ shipment-api/Infrastructure/Persistance/ShipContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shipment_api.Domain.Enitites;
+using shipment_api.Interfaces;
+
+namespace shipment_api.Infrastructure.Persistance
+{
+    public class ShipContext : DbContext, IShipContext
+    {
+        public ShipContext(DbContextOptions<ShipContext> options) : base(options)
+        {
+        }
+        public ShipContext() : base()
+        {
+        }
+        public DbSet<Package> Package { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Shipper> Shipper { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShipContext).Assembly);
+        }
+    }
+}
