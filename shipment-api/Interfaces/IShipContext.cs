4dc46219b64a7ae66f4f5250c49a673117968ed0
vszymon <vszymonwrobel@gmail.com>
2021-03-11 20:56:24 +0100

    feat: db context interface added

---
+++ shipment-api/Interfaces/IShipContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shipment_api.Domain.Enitites;
+using System;
+
+namespace shipment_api.Interfaces
+{
+    public interface IShipContext
+    {
+        public DbSet<Package> Package { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Shipper> Shipper { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
