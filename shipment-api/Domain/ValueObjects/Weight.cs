8fee7452b1d1aed9ca9aa35b2faa64667e64d449
vszymon <vszymonwrobel@gmail.com>
2021-03-11 20:56:07 +0100

    feat: value objects added with proper validation

---
+++ shipment-api/Domain/ValueObjects/Weight.cs
@@ -0,0 +1,25 @@
+﻿using shipment_api.Domain.Enums;
+using shipment_api.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValueOf;
+
+namespace shipment_api.Domain.ValueObjects
+{
+    public class Weight : ValueOf<(float, WeightUnit), Weight>
+    {
+        protected override void Validate()
+        {
+            if (Value.Item1 < 0) 
+            {
+                throw new InvalidWeightException(Value.Item1);
+            }
+        }
+        public override int GetHashCode()
+        {
+            return Value.Item1.GetHashCode() + Value.Item2.GetHashCode();
+        }
+    }
+}
---
+++ shipment-api/Domain/ValueObjects/ZipCode.cs
@@ -0,0 +1,34 @@
+﻿using shipment_api.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ValueOf;
+
+namespace shipment_api.Domain.ValueObjects
+{
+    public class ZipCode : ValueOf<string, ZipCode>
+    {
+        protected override void Validate()
+        {
+            try
+            {
+                var regex = new Regex(@"^([0-9]{2})-(\[[0-9]{3})", RegexOptions.CultureInvariant | RegexOptions.Singleline);
+
+                if (regex.IsMatch(Value) == false)
+                {
+                    throw new InvalidZipCodeException(Value);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                throw new InvalidEmailAddressException(Value);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                throw new InvalidEmailAddressException(Value);
+            }
+        }
+    }
+}
