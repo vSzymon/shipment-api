64565816235e19d5eec5a46f9a3af7addb6b5a01
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:12:05 +0200

    feat: IDateTime interface added

---
+++ shipment-api/Common/Interfaces/IDateTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shipment_api.Common.Interfaces
+{
+    public interface IDateTime
+    {
+        public DateTime Now { get; }
+    }
+}
