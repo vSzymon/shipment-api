42251f6d85d8b4140b13e428b528547a5aa4d91c
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:15:14 +0200

    reafactor: plural names

--- shipment-api/Infrastructure/Persistance/ShipContext.cs
+++ shipment-api/Infrastructure/Persistance/ShipContext.cs
@@ -12,10 +12,11 @@ namespace shipment_api.Infrastructure.Persistance
         public ShipContext() : base()
         {
         }
-        public DbSet<Package> Package { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Shipper> Shipper { get; set; }
-        public DbSet<Product> Product { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Label> Labels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
