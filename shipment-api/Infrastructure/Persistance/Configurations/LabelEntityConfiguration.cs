2ed5fe58e5d5ae5c0bf971b9d99aeb8fbe57bd10
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:14:19 +0200

    feat: entity configurations added

---
+++ shipment-api/Infrastructure/Persistance/Configurations/LabelEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using shipment_api.Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shipment_api.Infrastructure.Persistance.Configurations
+{
+    public class LabelEntityConfiguration : IEntityTypeConfiguration<Label>
+    {
+        public void Configure(EntityTypeBuilder<Label> builder)
+        {
+            builder.Property(prop => prop.LabelId).HasColumnName("Id");
+            builder.Property(prop => prop.Image).HasColumnName("Image");
+           
+            builder.Property(prop => prop.ImageFileType).HasColumnName("FileType");
+           
+            builder.HasOne(label => label.Package)
+                .WithOne(pack => pack.Label)
+                .HasForeignKey<Package>(pack => pack.LabelId)
+                .HasConstraintName("FK_packages_labels");
+        }
+    }
+}
---
+++ shipment-api/Infrastructure/Persistance/Configurations/PackageEntityConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using shipment_api.Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shipment_api.Infrastructure.Persistance.Configurations
+{
+    public class PackageEntityConfiguration : IEntityTypeConfiguration<Package>
+    {
+        public void Configure(EntityTypeBuilder<Package> builder)
+        {
+            builder.Property(prop => prop.PackageId).HasColumnName("Id");
+            builder.Property(prop => prop.TrackingNumber)
+                .HasColumnName("TrackingNumber")
+                .HasMaxLength(36);
+           
+            builder.Property(prop => prop.Created).HasColumnName("Created");
+            builder.Property(prop => prop.Shipped).HasColumnName("Shipped");
+            builder.Property(prop => prop.CustomerId).HasColumnName("CustomerId");
+            builder.Property(prop => prop.ShipperId).HasColumnName("ShipperId");
+            builder.Property(prop => prop.LabelId).HasColumnName("LabelId");
+
+            builder.HasMany(pack => pack.Products)
+                .WithOne(prod => prod.Package)
+                .HasForeignKey(prod => prod.PackageId)
+                .HasConstraintName("FK_packages_products");
+           
+            builder.HasOne(pack => pack.Label)
+                .WithOne(lab => lab.Package)
+                .HasForeignKey<Label>(pack => pack.LabelId)
+                .HasConstraintName("FK_packages_labels");
+          
+            builder.HasOne(pack => pack.Customer)
+                .WithMany(customer => customer.Packages)
+                .HasForeignKey(pack => pack.CustomerId)
+                .HasConstraintName("FK_packages_customers");
+
+            builder.HasOne(pack => pack.Shipper)
+                .WithMany(shipper => shipper.Packages)
+                .HasForeignKey(pack => pack.ShipperId)
+                .HasForeignKey("FK_packages_shippers");
+                
+        }
+    }
+}
