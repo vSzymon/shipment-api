8d7dad6f78e4ef14e1513c0a7ad770e82a2e36d7
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:16:07 +0200

    feat: mapper added

--- shipment-api/Labels/Queries/LabelViewModel.cs
+++ shipment-api/Labels/Queries/LabelViewModel.cs
@@ -1,8 +1,10 @@
-﻿using shipment_api.Domain.Enums;
+﻿using shipment_api.Common.Mappings;
+using shipment_api.Domain.Enitites;
+using shipment_api.Domain.Enums;
 
 namespace shipment_api.Labels.Queries
 {
-    public record LabelViewModel
+    public record LabelViewModel : IMapFrom<Label>
     {
         public byte[] Image { get; init; }
         public ImageFileType ImageFileType { get; init; }
