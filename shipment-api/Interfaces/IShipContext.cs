65ed3c0fc9934858828a30b5a125ee41195a4105
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:15:33 +0200

    refactor: plural names

--- shipment-api/Interfaces/IShipContext.cs
+++ shipment-api/Interfaces/IShipContext.cs
@@ -6,9 +6,10 @@ namespace shipment_api.Interfaces
 {
     public interface IShipContext
     {
-        public DbSet<Package> Package { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Shipper> Shipper { get; set; }
-        public DbSet<Product> Product { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Label> Labels { get; set; }
     }
 }
