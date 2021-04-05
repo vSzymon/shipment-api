ec7d6e305b6461fac80c0382b15220fff700d54d
vszymon <vszymonwrobel@gmail.com>
2021-04-05 17:12:56 +0200

    feat: aplly mappings for automapper

---
+++ shipment-api/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shipment_api.Common.Mappings
+{
+    interface IMapFrom<TEntity> where TEntity: class
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(TEntity), GetType());
+    }
+}
---
+++ shipment-api/Common/Mappings/Mappings.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using shipment_api.Common.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace shipment_api.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
